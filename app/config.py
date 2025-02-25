cdca6702b7586d5674f8a8d2fb99aab95ca7202a
Ella <ella.pldn2022@gmial.com>
2025-02-25 21:05:56 +0000

    Changed databse URI to work on Render & Docker

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 class Config:
     SECRET_KEY = '12345'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db' 
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class TestConfig(Config):
